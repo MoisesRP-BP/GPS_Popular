6edd79d002179b2ffe76fc51aecdeed49f5b60e5
moisesrobles04 <moises.robles@upr.edu>
2022-06-17 15:49:56 -0400

    Files Combined, no error for now

---
+++ automotive/src/main/java/intern/popular/gps_popular/ShowSavedLocationList.java
@@ -0,0 +1,39 @@
+package intern.popular.gps_popular;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.location.Location;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.io.BufferedReader;
+import java.io.IOError;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class ShowSavedLocationList extends AppCompatActivity {
+
+    ListView lv_savedLocations;
+    ;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_saved_location_list);
+
+        MyApplication myApplication= (MyApplication)getApplicationContext();
+        List<Location> savedLocations= myApplication.getMylocations();
+
+        lv_savedLocations= findViewById(R.id.lv_wayPoints);
+        lv_savedLocations.setAdapter(new ArrayAdapter<Location>(this, android.R.layout.simple_list_item_1, savedLocations));
+
+    }
+
+}
